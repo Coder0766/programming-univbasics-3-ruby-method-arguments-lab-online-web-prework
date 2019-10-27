0f29b5ba0347ee13c5c26d0584f79b45411073eb
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 04:06:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -22,14 +22,12 @@ introduction_with_language("Josh", "React")
 
 
 #Method takes in/uses Optional Arguments
-def introduction_with_language_optional(name="Edwin", language="React")
+def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
-introduction_with_language_optional
-introduction_with_language_optional("Josh")
-introduction_with_language_optional("Dan", "Ember.js")
-  
+introduction_with_language_optional("Edwin", "Ruby")
+
 
 
 
