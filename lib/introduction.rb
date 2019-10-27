3c509bd4428a19bd2d50640ffbad0179d2019075
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 03:49:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,12 +4,35 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-#introduction
-name = Courtney
-puts "Hi, my name is #{name}."
 
+#Method take in/uses an Argument
+def introduction(name= "Josh", "Dan")
+  puts "Hi, my name is #{name}."
+end
 
+introduction("Josh")
+introduction("Dan")
+  
 
+#Method takesin/uses Two Arguments
+def introduction_with_language(name="Josh", "Dan", language="React", "Ember.js" )
+  puts
+    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+    expect{introduction_with_language("Josh", "React")}.to output("Hi, my name is Josh and I am learning to program in React.\n").to_stdout
+  end
+end
+
+describe "#introduction_with_language_optional" do
+  it "takes in two arguments, a name and a language, and language defaults to Ruby" do
+    expect{introduction_with_language_optional("Edwin")}.to output("Hi, my name is Edwin and I am learning to program in Ruby.\n").to_stdout
+  end
+end
+
+describe "#introduction_with_language_optional" do
+  it "takes in two arguments, a name and a language, language can be optional or defined" do
+    expect{introduction_with_language_optional("Edwin", "Python")}.to output("Hi, my name is Edwin and I am learning to program in Python.\n").to_stdout
+  end
+end
 
 
 
